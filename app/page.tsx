563fc5c4f9fe9061de26b980da3eaabf7c0f52f0
luckyshanb <lbandara@infomedia.com.au>
2024-05-21 17:16:19 +0530

    feat: add basic changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm an experienced Software Engineer with a proven track record in 
+        designing and developing cutting-edge enterprise applications, 
+        proficient in Java.`}
       </p>
       <div className="my-8">
         <BlogPosts />
