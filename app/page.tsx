1aa8e903093fe4e432cb99c91ba34bea52adc371
luckyshanb <lbandara@infomedia.com.au>
2024-05-22 18:11:21 +0530

    feat: initial content changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+        hey, I'm Luckyshan Bandara 👋
       </h1>
       <p className="mb-4">
-        {`I'm an experienced Software Engineer with a proven track record in 
-        designing and developing cutting-edge enterprise applications, 
-        proficient in Java.`}
+        Welcome to My Creative Corner!
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      <p className="mb-4">
+        Dive into a world where creativity meets functionality. I'm a passionate Fullstack Software Engineer, and this space is a reflection of my journey and expertise. Here, you'll soon find a blend of my professional portfolio showcasing my latest projects, and a blog where I share insights, tips, and stories from my adventures in Software Engineering.
+      </p>
+      <p className="mb-4">
+        This site is currently a work in progress. Come back after a while to see the fully developed experience! Whether you're here to collaborate, get inspired, or simply explore, I hope you find something that resonates with you. Feel free to reach out, connect, and let’s create something amazing together!
+      </p>
+      <p>
+        Explore. Learn. Create.
+      </p>
     </section>
   )
 }
